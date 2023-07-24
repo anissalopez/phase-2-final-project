ef6db8049eca9366e98ba790364bc7056af52793
anissalopez <anissa965@gmail.com>
2023-07-24 15:38:02 -0400

    updated styling

--- my-app/src/Components/WeekData.js
+++ my-app/src/Components/WeekData.js
@@ -1,36 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { weekday } from "../weekdata";
 
 
 function WeekData({ habits}){
-
-
     let habitCount = {};
+
     const habitDetails = habits.forEach((habit) => {
-     habitCount[habit.habit] = 0
-      weekday.forEach((day) =>  {
-        if(habit[day]){
-            habitCount[habit.habit] = habitCount[habit.habit]  + 1
-        };
+     habitCount[habit.habit] = 0;
+      weekday.forEach((day) =>  {if(habit[day]){habitCount[habit.habit] = habitCount[habit.habit]  + 1};
       });
     });
 
-
-  const habitData = habits.map((habit) => {
-
-    const percent = `${(habitCount[habit.habit]/7 * 100).toFixed(2)}%`
+    const habitData = habits.map((habit) => {
+        const percent = `${(habitCount[habit.habit]/7 * 100).toFixed(2)}%`;
 
     return (
-        <div key={habit.habit}>
-        <p>{habit.habit}</p>
-        <p>Percentage of Goal: {habitCount[habit.habit] > 0 ?  percent : 0} </p>
-        </div>
+            <div className="circle" key={habit.habit}>
+                <p>{habit.habit}</p>
+                <p >{habitCount[habit.habit] > 0 ?  percent : `0%`} </p>
+            </div>
+           
     )
 })
 
 
     return (
-        <div>{habitData}</div>
+        <div className="data">{habitData}</div>
     )
 }
 
