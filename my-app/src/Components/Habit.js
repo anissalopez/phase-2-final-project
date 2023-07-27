e428e7f036cf451275cd48b2fa27d756a633c421
anissalopez <anissa965@gmail.com>
2023-07-27 15:38:08 -0400

    updating habit tracker

--- my-app/src/Components/Habit.js
+++ my-app/src/Components/Habit.js
@@ -1,73 +1,86 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt, faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { format } from "date-fns";
 
-function Habit({ habit, updateWeekDay, weekDays, removeHabit, refs}){
+function Habit({ habit, updateWeekDay, weekDays, removeHabit, refs }) {
 
-    const currentDay = new Date();
-    const [completedDays, setComplete] = useState([])
+  const currentDay = new Date();
+  const [completedDays, setComplete] = useState([]);
 
-    function handleClick(index){
 
-        console.log(refs.current[index].getDay())
-        console.log(currentDay.getDay())
 
-        
-        if(refs.current[index].getDay() > currentDay.getDay()){
-           alert("please check off a habit for the correct date")
+  function handleClick(index) {
+
+    
+    const convertedDates = habit.dates.map((day) => {
+        try {
+          return format((new Date(day)), "MM dd")
+        } catch (error) {
+          console.error("Error parsing date:", day);
+          return null;
         }
+      });
 
-
-        const filteredDates = completedDays.filter((day) => {
-           return day != currentDay
-        })
     
-        console.log(filteredDates)
 
-   
-      
-            fetch(`http://localhost:3000/habits/${habit.id}`, {
-            method: "PATCH",
-            headers: {
-                "content-type": "application/json",
-                "accept": "application/json"
-            },
-            body: JSON.stringify()
-        })
-        .then(resp => resp.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
     
-        
-    };
+  
+    if (convertedDates.includes(format((refs.current[index]), "MM dd"))) {
+      alert("You have already completed this habit for the selected day.");
+    } else {
+      if (refs.current[index].getDay() <= currentDay.getDay() && refs.current[index].getDay() > 0) {
+        const clickedDate = refs.current[index]
+        setComplete([...completedDays, clickedDate]);
 
-    function handleDelete(){
+        const updateDays = { dates: [...completedDays, clickedDate] };
+
         fetch(`http://localhost:3000/habits/${habit.id}`, {
-            method: 'DELETE',
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: JSON.stringify(updateDays)
         })
-        .then(resp => resp.json())
-        .then(() => removeHabit(habit.id))
+          .then(resp => resp.json())
+          .then(data => console.log(data))
+          .catch(error => console.log(error));
+      }else{
+        alert("You can not check off a future habit");
+      }
     }
+  }
 
-    const weekButtons = weekDays.map((day, index) => {
-        const completed = habit[day]? <FontAwesomeIcon icon={faCheck} /> : null
-            return(
-                <td key={day}>
-                    <input type="button"  className={habit[day] ? "btn btn-success" : "btn btn-outline-primary custom"} onClick={()=>handleClick(index)} />
-                        {completed}
-                </td>)
-    });
-     
+  function handleDelete() {
+    fetch(`http://localhost:3000/habits/${habit.id}`, {
+      method: 'DELETE',
+    })
+      .then(resp => resp.json())
+      .then(() => removeHabit(habit.id))
+  }
+
+  const weekButtons = weekDays.map((day, index) => {
+
+    const date = refs.current[index];
+    const completedDate = date
+    const isCompleted = completedDays.includes(completedDate);
+   
+    const completed = isCompleted ? <FontAwesomeIcon icon={faCheck} /> : null
     return (
-            <tr>
-            <th scope="row">{habit.habit}</th> 
-             {weekButtons}
-            <td><button onClick={handleDelete} className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt}/></button></td>
-            </tr>   
-    )
+      <td key={day}>
+        <input type="button" className={isCompleted ? "btn btn-success" : "btn btn-outline-primary custom"} onClick={() => handleClick(index)} />
+        {completed}
+      </td>)
+  });
+
+  return (
+    <tr>
+      <th scope="row">{habit.habit}</th>
+      {weekButtons}
+      <td><button onClick={handleDelete} className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt} /></button></td>
+    </tr>
+  )
 }
 
 export default Habit;
-
-
