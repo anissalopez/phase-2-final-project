2d3495839e63726d00b34d438d8aab104f9ed3eb
anissalopez <anissa965@gmail.com>
2023-07-27 08:30:21 -0400

    updating calendar format

--- my-app/src/Components/HabitContainer.js
+++ my-app/src/Components/HabitContainer.js
@@ -3,37 +3,20 @@ import Habit from "./Habit";
 import Header from "./Header";
 import { weekday } from "../weekdata";
 import { Table } from "react-bootstrap";
-import {
-  format,
-  addWeeks,
-  subWeeks,
-  startOfWeek,
-  lastDayOfWeek,
-  addDays
-} from "date-fns";
-
-
-
-
+import {format, addWeeks, subWeeks, startOfWeek,lastDayOfWeek, addDays} from "date-fns";
 
 function HabitContainer( {habits, updateWeekDay, removeHabit}){
 
-
-
-
-
-    const [activeDay, setActiveDay] = useState(new Date());
+  const [activeDay, setActiveDay] = useState(new Date());
   
-    const changeWeekHandle = (btnType) => {
+  const changeWeekHandle = (btnType) => {
       if (btnType === "prev") {
         setActiveDay(subWeeks(activeDay, 1));
-
-      }
+      };
       if (btnType === "next") {
         setActiveDay(addWeeks(activeDay, 1));
-      }
+      };
     };
-
  
   const dateHeader = () => {
       const dateFormat = "MMM yyyy";
@@ -42,24 +25,19 @@ function HabitContainer( {habits, updateWeekDay, removeHabit}){
       );
     };
   
-
-     
- 
-    const renderWeekDays = () => {
-    
+  const renderWeekDays = () => {
     let week = [];
-      
-  
-      const startDate = startOfWeek(activeDay, { weekStartsOn: 1 });
-      const endDate = lastDayOfWeek(activeDay, { weekStartsOn: 1 });
-      let currentDay = startDate
+
+    const startDate = startOfWeek(activeDay, { weekStartsOn: 1 });
+    const endDate = lastDayOfWeek(activeDay, { weekStartsOn: 1 });
+    let currentDay = startDate;
 
     for(let day = 0; day < 7; day++){
-      week.push(<th>{format(currentDay, "d")}</th>);
+      week.push(<th>{format(addDays(currentDay, day), "E")} {format(currentDay, "d")}</th>);
       currentDay = addDays(currentDay, 1);
       console.log(currentDay)
     }
-      return <tr>{week}</tr>  
+      return <>{week}</>  
 
        }
 
